aa373400a0344ba43eb95ffd7b5866f2886d36a3
Danielle Bergbower <dkw5011@gmail.com>
2020-03-17 21:56:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,9 +94,9 @@ def self.first_X_students_in_grade_10(n)
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT n
+      LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, n).map do |row|
       self.new_from_db(row)
 end
 end
